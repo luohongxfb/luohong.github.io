6268b38c20273f833a123c7a2ad52d38d02c2e7d
luohongxfb <luohongand@qq.com>
2019-03-21 11:19:03 +0800

    增加复制时自动追加copyright

---
+++ js/src/copyright.js
@@ -0,0 +1,28 @@
+(() => {
+  const author=document.getElementsByClassName("author")[0].textContent;
+    document.addEventListener('copy', e => {
+      let clipboardData = e.clipboardData || window.clipboardData;
+      if(!clipboardData) {
+        return;
+      }
+
+      e.preventDefault();
+
+      const selection = window.getSelection().toString();
+
+      const textData = selection + '\n\n'
+        + (author ? `作者: ${author}\n` : '')
+        + '原文: ' + window.location.href + '\n'
+        // + '来源: ' + window.location.host + '\n'
+        + '版权声明：本博客所有文章除特别声明外，均采用 CC BY-NC-SA 3.0 许可协议。转载请注明出处！\n\n';
+
+      const htmlData = selection + '<br/><br/>'
+        + (author ? `<b>作者</b>: ${author}<br/>` : '')
+        + `<b>原文</b>: <a href="${window.location.href}">${window.location.href}</a><br/>`
+        // + `<b>来源</b>: <a href="${window.location.origin}">${window.location.host}</a><br/>`
+        + '版权声明：本博客所有文章除特别声明外，均采用 <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY-NC-SA 3.0</a> 许可协议。转载请注明出处！<br/>';
+
+      clipboardData.setData('text/html', htmlData);
+      clipboardData.setData('text/plain', textData);
+    });
+})();
