d9c1d073cf492cea06f7e94161c91b493a9c5f2e
luohongxfb <luohongand@qq.com>
2019-07-12 12:29:33 +0800

    更改搭建文章布局

--- downloads/clipboard-use.js
+++ downloads/clipboard-use.js
@@ -3,7 +3,7 @@
   /* code */
   var initCopyCode = function () {
     var copyHtml = '';
-    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+    copyHtml += '<button class="btn-copy" data-clipboard-snippet>';
     copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
     copyHtml += '</button>';
     $(".highlight .code pre").before(copyHtml);
--- downloads/copyright.js
+++ downloads/copyright.js
@@ -16,10 +16,10 @@
         + '原文: ' + window.location.href + '\n'
         + '版权声明：本博客所有文章除特别声明外，均采用 CC BY-NC-SA 3.0 许可协议。转载请注明出处！\n\n';
 
-      const htmlData = selection + '<br/>-----------------------<br/>'
-        + (author ? `<b>作者</b>: ${author}<br/>` : '')
-        + `<b>原文</b>: <a href="${window.location.href}">${window.location.href}</a><br/>`
-        + '版权声明：本博客所有文章除特别声明外，均采用 <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY-NC-SA 3.0</a> 许可协议。转载请注明出处！<br/>';
+      const htmlData = selection + '<br>-----------------------<br>'
+        + (author ? `<b>作者</b>: ${author}<br>` : '')
+        + `<b>原文</b>: <a href="${window.location.href}">${window.location.href}</a><br>`
+        + '版权声明：本博客所有文章除特别声明外，均采用 <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank" rel="noopener">CC BY-NC-SA 3.0</a> 许可协议。转载请注明出处！<br>';
 
       clipboardData.setData('text/html', htmlData);
       clipboardData.setData('text/plain', textData);
